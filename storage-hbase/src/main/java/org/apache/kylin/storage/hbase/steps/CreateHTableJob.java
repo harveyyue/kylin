db21b3b6636869e7fdec2322bfd6cd5b5d2b9562
harveyyue <yw_yuewei@126.com>
2020-03-03 15:49:48 +0800

    add hbase configuration for flink engine

--- storage-hbase/src/main/java/org/apache/kylin/storage/hbase/steps/CreateHTableJob.java
+++ storage-hbase/src/main/java/org/apache/kylin/storage/hbase/steps/CreateHTableJob.java
@@ -121,7 +121,7 @@ public class CreateHTableJob extends AbstractHadoopJob {
         CubeHTableUtil.createHTable(cubeSegment, splitKeys);
 
         // export configuration in advance to avoid connecting to hbase from spark
-        if (cubeDesc.getEngineType() == IEngineAware.ID_SPARK || cubeDesc.getEngineType() == IEngineAware.ID_FLINK){
+        if (cubeDesc.getEngineType() == IEngineAware.ID_SPARK || cubeDesc.getEngineType() == IEngineAware.ID_FLINK) {
             exportHBaseConfiguration(cubeSegment.getStorageLocationIdentifier());
         }
         return 0;
